13546363c99239a696bcf4d83d3c9df3fcf66557
Marc Torres Sans <martorre@car6s2.42barcelona.com>
2023-11-13 19:49:25 +0100

    opa

--- main.c
+++ main.c
@@ -6,19 +6,101 @@
 /*   By: martorre <martorre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 15:21:24 by martorre          #+#    #+#             */
-/*   Updated: 2023/11/13 17:34:05 by martorre         ###   ########.fr       */
+/*   Updated: 2023/11/13 19:46:03 by martorre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 #include "./libft.h"
 
-void    check_file(char *str)
+int		check_borders_bott_lef(char **new)
 {
-    if(ft_strlen(ft_strnstr(str, ".bar", ft_strlen(str))) == ft_strlen(".bar"))
-        ft_printf("OOOOOKKKKKKKK");
+	int	y;
+	int	x;
+	int	out;
+
+	y = 0;
+	x = 0;
+	out = 0;
+	while (new[y] != NULL)
+		y++;
+	y--;		
+	while (new[y][x] != '\0' && out == 0)
+	{
+		if (new[y][x] != '1')
+			out = 1;
+		x++;
+	}
+	y = 0;
+	x = 0;
+	while (new[y][x] != '\n' && new[y][x] != '\0')
+		x++;
+	x--;
+	while (new[y] != NULL && out == 0)
+	{
+		if (new[y][x] != '1')
+			out = 1;
+		y++;
+	}
+	printf("%d", out);
+	return (out);
+}
+
+int	check_borders_top_ri(char **new)
+{
+	int	x;
+	int	y;
+	int	out;
+
+	x = 0;
+	y = 0;
+	out = 0;
+	/*while (new[0][x] != '\n' && out == 0)
+	{
+		if (new[0][x] != '1')
+			out = 1;
+		x++;
+	}*/
+	while (new[y] != NULL && out == 0)
+	{
+		if (ft_strcmp(new[y], "1") != 0)
+			out = 1;
+		y++;
+	}
+	//printf("%d", out);
+	check_borders_bott_lef(new);
+	return (out);
+}
+int	check_map(char **new)
+{
+	check_borders_top_ri(new);
+	return (0);
+}
+
+int	check_file(char *str)
+{
+	int		fd;
+	int		i;
+	char	*new[OPEN_MAX];
+
+	fd = 0;
+	i = 0;
+    if(ft_strlen(ft_strnstr(str, ".bar", ft_strlen(str))) != ft_strlen(".bar"))
+    	return (ft_printf("Invalid file!\n"), 1);
     else
-        ft_printf("Invalid file!\n");
+	{
+		fd = open(str, O_RDONLY);
+		if (fd == -1)
+			return (ft_printf("Invalid fd!\n"), 1);
+		new[i] = get_next_line(fd);
+		while (new[i] != NULL)
+		{
+			i++;
+			new[i] = get_next_line(fd);
+		}
+		check_map(new);
+	}
+	return (0);
 }
 int	main(int argc, char **argv)
 {
@@ -32,14 +114,9 @@ int	main(int argc, char **argv)
     if  (argc > 2)
         ft_printf("To many arguments!\n");
     else
-        check_file(argv[1]);
-	/*fd = open("hola", O_RDONLY);
-	new = get_next_line(fd);
-	while (new != NULL)
 	{
-		write(1,new, ft_strlen(new));
-		free(new);
-		new = get_next_line(fd);
-	}*/
+        check_file(argv[1]);
+
+	}
 	return (0);
 }
