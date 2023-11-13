8e1c62328d9c17b7caf4461adeeaece40de4ecd3
Marc Torres Sans <martorre@car6s2.42barcelona.com>
2023-11-13 17:34:46 +0100

    done

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: martorre <martorre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/12 15:33:33 by martorre          #+#    #+#             */
-/*   Updated: 2023/09/13 19:53:55 by martorre         ###   ########.fr       */
+/*   Updated: 2023/11/13 17:04:46 by martorre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,16 +38,16 @@ char	*ft_strnstr(const char *haystack, char *needle, size_t len)
 	}
 	if (needle[j] == '\0')
 		return ((char *)&haystack[i - j]);
-	return (0);
+	return (NULL);
 }
-/*
-int	main(void)
+
+/*int	main(void)
 {
-	char str[] = "hola";
-	char find[] = "la";
+	char str[] = "hola.bar";
+	char find[] = ".bar";
 	char *re;
-	re = ft_strnstr(str, find, 2);
-	// re = strstr(str,find);
+	re = ft_strnstr(str, find, ft_strlen(str));
+	//re = strnstr(str,find, 2);
 	printf("STR = %s\n", str);
 	printf("FIND = %s\n", find);
 	printf("Return = %s", re);
--- main.c
+++ main.c
@@ -6,7 +6,40 @@
 /*   By: martorre <martorre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 15:21:24 by martorre          #+#    #+#             */
-/*   Updated: 2023/11/09 15:21:25 by martorre         ###   ########.fr       */
+/*   Updated: 2023/11/13 17:34:05 by martorre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "so_long.h"
+#include "./libft.h"
+
+void    check_file(char *str)
+{
+    if(ft_strlen(ft_strnstr(str, ".bar", ft_strlen(str))) == ft_strlen(".bar"))
+        ft_printf("OOOOOKKKKKKKK");
+    else
+        ft_printf("Invalid file!\n");
+}
+int	main(int argc, char **argv)
+{
+	/*int fd;
+	char *new;
+	int i;
+	int	num;
+
+	i = 0;*/
+    //(void) argv;
+    if  (argc > 2)
+        ft_printf("To many arguments!\n");
+    else
+        check_file(argv[1]);
+	/*fd = open("hola", O_RDONLY);
+	new = get_next_line(fd);
+	while (new != NULL)
+	{
+		write(1,new, ft_strlen(new));
+		free(new);
+		new = get_next_line(fd);
+	}*/
+	return (0);
+}
